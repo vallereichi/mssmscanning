596970643edf4b242750c51271782ab66e6b0216
Valentin Reichenspurner <v.reichenspurner@tum.de>
2025-10-09 17:17:17 +0200

    add pixi as version management

--- Util/merge_hdf5.py
+++ Util/merge_hdf5.py
@@ -10,32 +10,26 @@ import numpy as np
 import vallog as vl
 
 msg = vl.Logger()
-outfile = open("merge.log", "w", encoding="utf-8")
 
 
-def scan_info(scan: dict[str, np.ndarray], file_path: str, file_type: str) -> None:
+def scan_info(scan: dict[str, np.ndarray], file_path: str, file_type: str, logfile) -> None:
     """
     recieve some inforamation about a scan and keep track of it in the logs
     """
-    outfile.write(f"found {len(scan)} keys in scan {file_path}\n")
+    logfile.write(f"found {len(scan)} keys in scan {file_path}\n")
     # create filters for valid models
     if file_type == "hdf5":
         try:
             mask = scan["LogLike_isvalid"] == 1
             n_valid_models = sum(mask)
         except KeyError:
-            outfile.write("WARNING: 'LogLike_isvalid' key not found, no filter for valid models can be applied\n")
-        try:
-            mask = scan["GM2_gmuon"] != -1
-            n_valid_models = sum(mask)
-        except KeyError:
-            outfile.write("WARNING: 'GM2_gmuon' key not found, no filter for valid models can be applied\n")
+            logfile.write("WARNING: 'LogLike_isvalid' key not found, no filter for valid models can be applied\n")
     elif file_type == "root":
         try:
             mask = scan["GM2_gmuon"] != -1
             n_valid_models = sum(mask)
         except KeyError:
-            outfile.write("WARNING: 'GM2_gmuon' key not found, no filter for valid models can be applied\n")
+            logfile.write("WARNING: 'GM2_gmuon' key not found, no filter for valid models can be applied\n")
     else:
         raise ValueError(f"FileType '{file_type}' is not supported")
 
@@ -43,52 +37,52 @@ def scan_info(scan: dict[str, np.ndarray], file_path: str, file_type: str) -> No
     f = len(scan[next(iter(scan))])
     if all(len(x) == f for x in scan.values()):
         if "n_valid_models" in locals():
-            outfile.write(f"\tVALID MODELS: {n_valid_models}/{f}\n")
+            logfile.write(f"\tVALID MODELS: {n_valid_models}/{f}\n")
     else:
-        outfile.write("WARNING: keys have different lengths\n")
+        logfile.write("WARNING: keys have different lengths\n")
         if "n_valid_models" in locals():
-            outfile.write(f"\tVALID MODELS: {n_valid_models}/{f}\n")
+            logfile.write(f"\tVALID MODELS: {n_valid_models}/{f}\n")
         for key, value in scan.items():
-            outfile.write(f"\tKEY: {key} SHAPE: {value.shape}\n")
+            logfile.write(f"\tKEY: {key} SHAPE: {value.shape}\n")
 
 
-def search_hdf5_files(directory: str) -> list[str]:
+def search_hdf5_files(directory: str, logfile) -> list[str]:
     """
     recursivly search the directory for hdf5 files and return the found paths in a list
     """
     msg.heading(f"searching for hdf5 files in '{directory}' ...")
-    outfile.write(f"searching for hdf5 files in '{directory}' ...\n")
+    logfile.write(f"searching for hdf5 files in '{directory}' ...\n")
     hdf5_files = glob.glob(os.path.join(directory, "**", "*.hdf5"), recursive=True)
     if not hdf5_files:
         msg.log(f"No hdf5 files found in '{directory}'", vl.error)
-        outfile.write(f"No hdf5 files found in '{directory}'\n")
+        logfile.write(f"No hdf5 files found in '{directory}'\n")
         exit(1)
     for file in hdf5_files:
         msg.log(f"found: {file}", vl.info)
-        outfile.write(f"found: {file}\n")
-    outfile.write(f"found {len(hdf5_files)} hdf5 files\n\n")
+        logfile.write(f"found: {file}\n")
+    logfile.write(f"found {len(hdf5_files)} hdf5 files\n\n")
     return hdf5_files
 
 
-def search_root_files(directory: str) -> list[str]:
+def search_root_files(directory: str, logfile) -> list[str]:
     """
     recursivly search the directory for root files and return the found paths in a list
     """
     msg.heading(f"searching for root files in '{directory}' ...")
-    outfile.write(f"searching for root files in '{directory}' ...\n")
+    logfile.write(f"searching for root files in '{directory}' ...\n")
     root_files = glob.glob(os.path.join(directory, "**", "*.root"), recursive=True)
     if not root_files:
         msg.log(f"No root files found in '{directory}'", vl.error)
-        outfile.write(f"No root files found in '{directory}'\n")
+        logfile.write(f"No root files found in '{directory}'\n")
         exit(1)
     for file in root_files:
         msg.log(f"found: {file}", vl.info)
-        outfile.write(f"found: {file}\n")
-    outfile.write(f"found {len(root_files)} hdf5 files\n\n")
+        logfile.write(f"found: {file}\n")
+    logfile.write(f"found {len(root_files)} hdf5 files\n\n")
     return root_files
 
 
-def read_hdf5_file(file_path: str) -> dict:
+def read_hdf5_file(file_path: str, logfile) -> dict:
     """
     extract the data from the specified hdf5 file and return it as a dictionary
     """
@@ -100,11 +94,11 @@ def read_hdf5_file(file_path: str) -> dict:
         else:
             raise TypeError(f"expected type 'h5py.Group' but got {type(dataset)}")
     msg.log(f"read {len(data_dict)} keys from '{file_path}'", vl.info)
-    scan_info(data_dict, file_path, "hdf5")
+    scan_info(data_dict, file_path, "hdf5", logfile)
     return data_dict
 
 
-def read_root_file(file_path: str) -> dict:
+def read_root_file(file_path: str, logfile) -> dict:
     """
     extract the data from the specified root file and return it as a dictionary
     """
@@ -112,11 +106,11 @@ def read_root_file(file_path: str) -> dict:
     dataset = uproot.open(file_path + ":susy")
     data_dict = {key: np.array(dataset[key]) for key in dataset.keys()}
     msg.log(f"read {len(data_dict)} keys from '{file_path}'", vl.info)
-    scan_info(data_dict, file_path, "root")
+    scan_info(data_dict, file_path, "root", logfile)
     return data_dict
 
 
-def merge_datasets(datasets: list[dict]) -> dict:
+def merge_datasets(datasets: list[dict], logfile) -> dict:
     """
     merge multiple datasets into one dictionary
     """
@@ -133,7 +127,7 @@ def merge_datasets(datasets: list[dict]) -> dict:
     for key in merged_data:
         merged_data[key] = np.concatenate(merged_data[key], axis=0)
     msg.log(f"merged {len(datasets)} datasets", vl.info)
-    scan_info(merged_data, "merged_dataset", "hdf5")
+    scan_info(merged_data, "merged_dataset", "hdf5", logfile)
     return merged_data
 
 
@@ -151,20 +145,20 @@ def save_dataset_to_hdf5(dataset: dict, output_path: str) -> None:
 
 def main(directory: str, output_filename: str, file_type: str):
     """Entry point"""
-    outfile_path = directory + "/merge.log"
+    logfile_path = directory + "/merge.log"
+    logfile = open(logfile_path, 'w')
     if file_type == "hdf5":
-        hdf5_files = search_hdf5_files(directory)
-        datasets = [read_hdf5_file(file) for file in hdf5_files]
+        hdf5_files = search_hdf5_files(directory, logfile)
+        datasets = [read_hdf5_file(file, logfile) for file in hdf5_files]
     elif file_type == "root":
-        root_files = search_root_files(directory)
-        datasets = [read_root_file(file) for file in root_files]
+        root_files = search_root_files(directory, logfile)
+        datasets = [read_root_file(file, logfile) for file in root_files]
     else:
         raise ValueError(f"FileType '{file_type}' is not supported")
 
-    merged_data = merge_datasets(datasets)
+    merged_data = merge_datasets(datasets, logfile)
     save_dataset_to_hdf5(merged_data, directory + "/" + output_filename)
-    outfile.close()
-    os.rename("./merge.log", outfile_path)
+    logfile.close()
 
 
 if __name__ == "__main__":
